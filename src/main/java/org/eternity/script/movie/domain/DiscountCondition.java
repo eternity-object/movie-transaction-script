ea832cc66dcc591b7955e7a1fed47ad3f2d79cd8
snatcher93 <snatcher93@hanmail.net>
2024-12-03 13:10:58 +0900

    식별자와 열거형 매핑 예제

--- src/main/java/org/eternity/script/movie/domain/DiscountCondition.java
+++ src/main/java/org/eternity/script/movie/domain/DiscountCondition.java
@@ -1,21 +1,28 @@
 package org.eternity.script.movie.domain;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
+@Entity
+@SequenceGenerator(
+        name = "discount_seq",
+        sequenceName = "discount_seq",
+        initialValue = 1, allocationSize = 50
+)
 @NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
 @Getter @Setter
 public class DiscountCondition {
     public enum ConditionType { PERIOD_CONDITION, SEQUENCE_CONDITION }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "discount_sequence")
     private Long id;
     private Long policyId;
-
     private ConditionType conditionType;
     private DayOfWeek dayOfWeek;
-
     private LocalTime startTime;
     private LocalTime endTime;
     private Integer sequence;
